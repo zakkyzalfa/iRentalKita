2d40cf615fe3a6deadfa2f9ee1529b71177df069
zakkyzalfa <zakkyzalfa16@gmail.com>
2025-08-07 02:56:35 +0700

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - iRentalKita</title>
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png"/>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
